cc96577a76aca1c9ac6b07a8bd1f647b64964f83
sriramyadav860-beep <sriramyadav860@gmail.com>
2025-12-10 18:53:31 +0530

    Your message about the changes

--- jussst.py
+++ jussst.py
@@ -1,3 +1,34 @@
 s = "hello"
 reversed_s = s[::-2]
 print(reversed_s)  
+
+num = 9
+
+if num > 0:
+    print("The number is positive")
+elif num < 0:
+    print("The number is negative")
+else:
+    print("The number is zero")
+
+x = 10
+name = "Sriram"
+price = 99.5
+print(x)
+print(name)
+print(price)
+
+a = 16
+b = 16
+
+if a == b:
+    print("Both numbers are equal")
+else:
+    print("The numbers are not equal")
+
+age = 20
+if age >=18 and age <=20:
+    print("eligible")
+else:
+    print("not eligible")
+    
